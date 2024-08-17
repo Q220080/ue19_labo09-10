0feb4d5f4d5da3277627ea7adbe562b5b81bc23a
Antolo <113942324+Q220080@users.noreply.github.com>
2024-08-17 10:48:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import requests
 
+def get_pun():
+    url = "https://v2.jokeapi.dev/joke/Any?type=single"  # URL d'une API publique de blagues
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        joke_data = response.json()
+        return joke_data.get("joke", "Aucune blague trouvée!")
+    else:
+        return "Impossible d'obtenir une blague."
+
+if __name__ == "__main__":
+    print("Voici une blague :")
+    print(get_pun())
